630455b00b56ae6a622cd9064f38938313a10d6c
Chris Diniz <chris.diniz.dev@outlook.com>
2026-06-25 01:17:20 -0300

    chore: initial project structure
    Sets up Electron project base with package.json (electron + electron-builder),
    main.js as entry point and .gitignore ignoring node_modules/ and dist/.

---
+++ main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    minWidth: 960,
+    minHeight: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    },
+    title: 'Karaoke Shake',
+    autoHideMenuBar: true
+  })
+
+  win.loadFile(path.join(__dirname, 'src', 'index.html'))
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  app.quit()
+})
